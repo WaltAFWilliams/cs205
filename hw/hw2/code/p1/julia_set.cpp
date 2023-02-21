8d01cf3818e9e1b9bf714e2fecdf96e1572ce988
student u451408g113711 group g113711 class COMPSCI205 spring2023 <u451408g113711@academic-login06.rc.fas.harvard.edu>
2023-02-21 12:54:49 -0500

    added initial hw2 files

---
+++ hw/hw2/code/p1/julia_set.cpp
@@ -0,0 +1,72 @@
+// File       : julia_set.cpp
+// Description: Code for Julia Set problem CS205
+// Copyright 2023 Harvard University. All Rights Reserved.
+#include "helper.h"
+
+#include <chrono>
+#include <omp.h>
+
+constexpr int MAX_ITERATIONS = 1000;
+short image[HEIGHT][WIDTH];
+
+/*
+ * Generate the Julia set.
+ *
+ * Fill each pixel `image[iy][ix]` with the number of iterations for the point
+ * (x, y).
+ */
+void julia_set(void)
+{
+    // Some parameters of the Julia set (keep them for your final submission)
+    constexpr double cx = -0.624;
+    constexpr double cy = 0.435;
+
+    auto t0 = std::chrono::steady_clock::now();
+
+    // TODO A: parallelize this code
+    for (int i = 0; i < HEIGHT; ++i) {
+        for (int j = 0; j < WIDTH; ++j) {
+            // Compute `w = z_0 = x + i y` for the given pixel (j, i).
+            double x = 1.3 * (j - .5 * WIDTH) / HEIGHT;
+            double y = 1.3 * i / HEIGHT; // Only one half.
+
+            // Compute the count iterations for this pixel.
+            int count = 0;
+            // TODO A: implement the iteration rule here
+
+            image[i][j] = count;
+        }
+    }
+    auto t1 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> diff = t1 - t0;
+    printf("Computing done in %.2lfs\n", diff.count());
+}
+
+/*
+ * Compute the histogram of iteration count.
+ *
+ * Returns an `std::vector<int>`, where `i`-th elements represents the number
+ * of pixels that had `i` iterations.
+ */
+std::vector<int> compute_histogram(void)
+{
+    std::vector<int> result(MAX_ITERATIONS + 1);
+
+    // TODO D: Compute the histogram of iteration count.
+    //         Parallelize your code with OpenMP.
+    //
+    //         To report your result, you can run `make plot`
+    //         to plot the computed histogram.
+
+    return result;
+}
+
+int main(void)
+{
+    julia_set();
+    save_image(&image[0][0]);
+
+    std::vector<int> histogram = compute_histogram();
+    save_histogram(histogram);
+    return 0;
+}
