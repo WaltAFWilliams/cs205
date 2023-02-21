37a5078605e7b7329b661cad0edae4bdf9a8dc1b
Walt Williams <amboufaye1@gmail.com>
2023-02-21 06:53:30 -0500

    finished all of problem 1

---
+++ lab/lab2/code/t1/plot_times.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.figure()
+x = np.array([1,2,4,6,16,28])
+y = np.array([0.035, 0.103, 0.143, 0.193, 0.308, 0.478])
+plt.scatter(x,y)
+plt.xlabel('number of threads')
+plt.ylabel('t(s)')
+plt.grid()
+plt.title('Performance vs thread count')
+plt.show()
